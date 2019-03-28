b5e5b23438b5d11469b1866787cc163b10cc2e56
Zach Freund <theeuskon@gmail.com>
2019-03-28 15:15:05 -0500

    more edits php

--- CreateUser.php
+++ CreateUser.php
@@ -1,5 +1,4 @@
 <?php
-  echo "Hello";
   $dbServer = "mysql.eecs.ku.edu";
   $dbUsername = "z148f106";
   $dbPW = "Eith9aeF";
@@ -11,24 +10,20 @@
       exit();
   }
 
-  $userID = "";
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userID = $_POST["user_id"];
-    if(empty($userID))
-    {
-      echo "Error: User ID Field Blank. <br>";
+  $userID = $_POST["user_id"];
+  if(empty($userID))
+  {
+    echo "Error: User ID Field Blank. <br>";
 
+  } else {
+
+    $query = "INSERT INTO Users (user_id) VALUES ($userID)";
+    if($db->query($query)) {
+      echo "User Added to Database. <br>";
     } else {
-
-      $query = "INSERT INTO Users (user_id) VALUES ($userID)";
-      if($db->query($query)) {
-        echo "User Added to Database. <br>";
-      } else {
-        echo "<script type='text/javascript'>alert("User NOT Added, Redirecting.");</script>";
-        echo "<script type='text/javascript'>window.location.href = "CreateUser.html";</script>";
-      }
+      echo "<script type='text/javascript'>alert("User NOT Added, Redirecting.");</script>";
+      echo "<script type='text/javascript'>window.location.href = "CreateUser.html";</script>";
     }
-
   }
   $db->close();
  ?>
