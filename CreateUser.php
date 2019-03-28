1a27d3ed5260a23fdf71ee444d2fd76374b0be60
Zach Freund <theeuskon@gmail.com>
2019-03-28 14:27:54 -0500

    Create User implemented.

---
+++ CreateUser.php
@@ -0,0 +1,33 @@
+<?php
+$dbServer = "mysql.eecs.ku.edu";
+$dbUsername = "z148f106";
+$dbPW = "Eith9aeF";
+$dbName = "z148f106";
+$db = new mysqli($dbServer, $dbUsername, $dbPW);
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$userID = "";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $userID = $_POST["user_id"];
+  if(empty($userID))
+  {
+    echo "Error: User ID Field Blank. <br>";
+
+  } else {
+
+    $query = "INSERT INTO Users (user_id) VALUES ($userID)";
+    if($db->query($query)) {
+      echo "User Added to Database. <br>";
+    } else {
+      echo "<script type='text/javascript'>alert("User NOT Added, Redirecting.");</script>";
+      echo "<script type='text/javascript'>window.location.href = "CreateUser.html";</script>";
+    }
+  }
+
+}
+
+ ?>
