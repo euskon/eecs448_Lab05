80986e7483ab3215e09baa4c0a4a27a0356d798b
Zach Freund <z148f106@ku.edu>
2019-03-29 00:56:35 -0500

    completed Posting portion of lab

---
+++ CreatePosts.php
@@ -0,0 +1,35 @@
+<?php
+  /*connect*/
+  ini_set('display_errors', 1);
+  $dbServer = "mysql.eecs.ku.edu";
+  $dbUsername = "z148f106";
+  $dbPW = "Eith9aeF";
+  $dbName = "z148f106";
+  $db = new mysqli($dbServer, $dbUsername, $dbPW, $dbName);
+  /* check connection */
+  if ($db->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+  /*extract and use data*/
+  $content = $_POST["content"];
+  $userID = $_POST["user_id"];
+  if(empty($userID)) {
+    echo "Error: User ID Field Blank. <br>";
+    $db->close();
+    exit();
+  }
+  if(empty($content)) {
+    echo "Error: Conent Field Blank. <br>";
+    $db->close();
+    exit();
+  }
+  $query = "INSERT INTO Posts (post_id, content, author_id) VALUES (NULL, '$content', '$userID')";
+  if($db->query($query) === true) {
+    echo "Post Submitted. <br>";
+  } else {
+    echo "<script type='text/javascript'>alert('failed to Added, Redirecting.');</script>";
+    echo "<script type='text/javascript'>window.location.href = 'CreatePosts.html';</script>";
+  }
+  $db->close();
+ ?>
